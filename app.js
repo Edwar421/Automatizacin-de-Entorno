83ad789ee24f266188f484f6407b5a4dd9dca82e
Edwar Santiago Vargas Rivera <esvargasr@udistrital.edu.co>
2025-07-23 23:16:17 -0500

    Mejora Visualizacion

--- app.js
+++ app.js
@@ -1,12 +1,17 @@
 const express = require('express');
+const path = require('path');
 const app = express();
 const port = 3000;
 
-// Rutas
-app.use('/', require('./Routes/index'));
-app.use('/about', require('./Routes/about'));
-app.use('/status', require('./Routes/status'));
+// Servir archivos estáticos (CSS, imágenes, etc.)
+app.use(express.static(path.join(__dirname, 'public')));
 
+// Rutas
+app.use('/', require('./routes/index'));
+app.use('/about', require('./routes/about'));
+app.use('/status', require('./routes/status'));
+
+// Iniciar servidor
 app.listen(port, () => {
   console.log(`Servidor corriendo en http://localhost:${port}`);
 });
